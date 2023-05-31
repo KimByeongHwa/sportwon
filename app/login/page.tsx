be7117f59b970058f93f581ff1f18f7716a18878
KimByeongHwa <iambyeonghwa@gmail.com>
2023-05-31 17:46:08 +0900

    edit .eslintrc.json

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import styled from 'styled-components'
+
+function Login() {
+  return <Container>로그인 페이지</Container>
+}
+
+const Container = styled.div`
+  border: 1px solid black;
+`
+
+export default Login
---
+++ app/signup/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import styled from 'styled-components'
+
+function SignUp() {
+  return <Container>회원가입 페이지</Container>
+}
+
+const Container = styled.div`
+  border: 1px solid black;
+`
+
+export default SignUp
